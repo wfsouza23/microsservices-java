ef65ce5185208202d408fb1f769c2942de3c991c
Wesley de Souza <wfsouza23@gmail.com>
2024-12-19 20:03:12 -0300

    microsservices pagamentos

---
+++ pagamentos/src/test/java/br/com/microsservices_java/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microsservices_java.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
